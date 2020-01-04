163ccaae28f67f7ba20207d88b34244f81d9e0bf
hkasafwat <hkasafwat@gmail.com>
2020-01-04 20:22:39 +0000

    stopwatch with play, pause and stop | minutes is changeable seconds are not. need to fix start after pause functionality

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
 let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
 
-let stopwatch = setInterval(function() {
-   if(minutes.innerHTML != 0 || minutes.innerHTML != '0') {
-        minutes.innerHTML--;
-        console.log(minutes)
-   } else {
-       clearInterval(stopwatch);
-   }
-}, 100);
+let start = document.querySelector('.start');
+let pause = document.querySelector('.pause');
+let stop = document.querySelector('.stop');
+
+let minutesStopwatch;
+let secondsStopwatch;
+
+let hasBeenPause = false;
+
+function aSecond(minutes, seconds) {
+    seconds.innerHTML = 59;
+
+    secondsStopwatch = setInterval(function () {
+        if (seconds.innerHTML != 0 || seconds.innerHTML != '0') {
+            seconds.innerHTML--;
+        }
+
+        if(seconds.innerHTML == 0 && minutes.innerHTML != 0) {
+            minutes.innerHTML--;
+            clearInterval(secondsStopwatch)
+            aSecond(minutes, seconds)
+        }
+
+        if(seconds.innerHTML == 0 && minutes.innerHTML == 0) {
+            clearInterval(secondsStopwatch)
+            return;
+        }
+
+    }, 100)
+}
+
+start.addEventListener('click', function () {
+    if(isNaN(minutes.innerHTML)) {
+        console.log('not a number')
+        alert('Please insert a valid number');
+        return;
+    }
+    
+    minutes.innerHTML--;
+    aSecond(minutes, seconds);
+});
+
+pause.addEventListener('click', function () {
+    clearInterval(secondsStopwatch);
+})
+
+stop.addEventListener('click', function () {
+    clearInterval(secondsStopwatch);
+    minutes.innerHTML = 25;
+    seconds.innerHTML = '00';
+})
